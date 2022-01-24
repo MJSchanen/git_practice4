915b8c5adcfbf93575d49475615e32caa72ff389
MJSchanen <mjschanen@gmail.com>
2022-01-24 09:32:50 -0600

    1st commit

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -1,5 +1,9 @@
 package Hello;
 
 public class Hello {
+    public static void main(String[] args) {
+        //do not need to have main BUT you need to comment out if you don't
+    }
+//World
 
 }
